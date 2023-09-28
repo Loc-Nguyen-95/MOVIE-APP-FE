7d7df02b6975d60f025f859359ada8864d84c52f
Nguyễn Phúc Lộc <locnpfx13001@funix.edu.vn>
2023-09-28 16:56:21 +0700

    search + css search page

---
+++ src/components/search-result/SearchResult.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './searchresult.css';
+
+function SearchResult() {
+  return (
+    <div>
+      This is result
+    </div>
+  )
+}
+
+export default SearchResult
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './search.css';
 
 function Search() {
+  const [searchValue, setSearchValue] = useState(''); // value input = text
+  const [query, setQuery] = useState('');
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
+  }
+
+  const handleSearch = () => {
+    setQuery(searchValue);
+  }
+
+
   return (
-    <div>
-      hello from seach page
+    <div className='search-page'>
+
+      <div className='search-container'>
+        <form
+          onSubmit={e => e.preventDefault()}  //ngăn k load lại
+        >
+              <div className='search-input'>
+                <input
+                  type="text"
+                  placeholder='Type keyword'
+                  value={searchValue}
+                  onChange={handleChange}
+                />
+              </div>
+
+                <div className='search-button'>
+                  <button
+                    className='btn-search'
+                    onClick={handleSearch}>
+                    SEARCH
+                  </button>
+                </div>
+
+        </form>
+      </div>
+
+      {/* search-result */}
+
     </div>
   )
 }
